423812e8afd5124568918d60d5098430c693f00a
Yiwen <yiwenxu20@outlook.com>
2021-08-28 12:35:16 -0500

    Add a label.

---
+++ Digits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Digits
+//
+//  Created by 徐艺文 on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
